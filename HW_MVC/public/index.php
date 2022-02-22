48fc21a71822de07f8ea04869271648b7a0d9441
Gadzhi <91538968+Gadzhizhka@users.noreply.github.com>
2022-02-22 14:07:52 +0300

    Последнее задание

---
+++ HW_MVC/public/index.php
@@ -0,0 +1,7 @@
+<?php
+use Cakes\Kernel\Router;
+require_once '../vendor/autoload.php';
+if (preg_match('/\.(css|js)$/', $_SERVER["REQUEST_URI"])) {
+  return false;
+}
+Router::start();
---
+++ HW_MVC/src/Controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+namespace Cakes\Controllers;
+use Cakes\Kernel\Controller;
+class IndexController extends Controller {
+  public function index(){
+    echo $this->getIndex();
+  }
+}
